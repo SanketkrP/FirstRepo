da0dc2e0376a63d1ba3300c03beb8fed1272248f
SanketkrP <justforstudies133@gmail.com>
2025-01-02 20:03:33 +0530

    Random commit
    This is a first commit of this year.

---
+++ newcppfirle.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(vector<int> val,int h)
+{
+    int n=val.size(),c=0;
+    for(int i=0;i<n;i++)
+    {
+        if(val[i]<=h)
+        {
+            c=c+1;
+        }
+        else
+        {
+          c=c+2;  
+        }
+    }
+    
+    cout<<c<<endl;
+}
+
+int main()
+{
+    int n,h;
+    cin>>n>>h;
+    vector<int> val(n);
+    int i=0;
+    while(n--)
+    {
+        cin>>val[i];
+        i++;
+    }
+    solve(val,h);
+    return 0;
+}
